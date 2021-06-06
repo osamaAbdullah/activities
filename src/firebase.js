f29d80b72b8a8ab19d2d37f289eca79ccf931d44
osama <osama@akre.com>
2021-06-06 19:00:44 +0300

    new pro

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,9 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
-firebase.initializeApp({
-    apiKey: "AIzaSyCNrkAkLzcGOCCHLK1JUskC8sAJcmXN2Sw",
-    authDomain: "activities-c8c9f.firebaseapp.com",
-    projectId: "activities-c8c9f",
-    storageBucket: "activities-c8c9f.appspot.com",
-    messagingSenderId: "1003081335802",
-    appId: "1:1003081335802:web:1ed7f1d694411766db5a43"
-});
+import {firebaseConf} from "../conf";
+
+firebase.initializeApp(firebaseConf);
 
 export const db = firebase.firestore();
 export const auth = firebase.auth();
