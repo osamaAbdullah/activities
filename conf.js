966c1b5e3d223f761ef9ece9ad81fe8f02667bf2
osama <osama@akre.com>
2021-06-11 23:03:47 +0300

    change to production DB and build

--- conf.js
+++ conf.js
@@ -2,10 +2,10 @@
 export
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0NKEO-ZujRilEygjxRrduzhlmz_FuI0A",
-    authDomain: "activities-test-9f80a.firebaseapp.com",
-    projectId: "activities-test-9f80a",
-    storageBucket: "activities-test-9f80a.appspot.com",
-    messagingSenderId: "13877941131",
-    appId: "1:13877941131:web:c917ff43079dab07f6958e"
+    apiKey: "AIzaSyD-Ny42JvupIsCpgUSgYu__IcZry0ZEC8o",
+    authDomain: "fun-act.firebaseapp.com",
+    projectId: "fun-act",
+    storageBucket: "fun-act.appspot.com",
+    messagingSenderId: "545284794512",
+    appId: "1:545284794512:web:ffef95f1067025f7861015"
 };
