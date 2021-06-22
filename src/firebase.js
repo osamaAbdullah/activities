19ca4e0d62616738b68d8950114efc3e2b42f2dd
osama <osama@akre.com>
2021-06-22 11:43:04 +0300

    fb fcm

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@ import firebase from "firebase/app";
 
 import "firebase/analytics";
 
+import '@firebase/messaging';
+
 import "firebase/auth";
 import "firebase/firestore";
 
@@ -11,7 +13,57 @@ firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
 export const auth = firebase.auth();
+export const messaging = firebase.messaging();
 
+
+messaging
+    .getToken({vapidKey: 'BIzxd60H-rb9VzB1v_n89BZ0Zj01E0DVDr8BKEHycdSBBCbUqWVdzj6mUl6IHD1I_RDEqD24rN8dsJnMB9b00T4'})
+    .then((currentToken) => {
+        if (currentToken) {
+            // Send the token to your server and update the UI if necessary
+            // ...
+            console.log(currentToken, 'here');
+        } else {
+            // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.');
+            // ...
+        }
+    }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
+
+
+messaging.onMessage(function (payload) {
+    console.log(payload);
+    const notificationOption = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+
+    if (Notification.permission === "granted") {
+        var notification = new Notification(payload.notification.title, notificationOption);
+
+        notification.onclick = function (ev) {
+            ev.preventDefault();
+            window.open(payload.notification.click_action, '_blank');
+            notification.close();
+        }
+    }
+
+})
+
+// messaging.onTokenRefresh(function () {
+//     messaging.getToken()
+//         .then(function (newtoken) {
+//             console.log("New Token : "+ newtoken);
+//         })
+//         .catch(function (reason) {
+//             console.log(reason);
+//         })
+// })
+
+// IntitalizeFireBaseMessaging();
 
 
 
