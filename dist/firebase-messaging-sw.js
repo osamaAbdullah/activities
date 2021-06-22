8d8ef0609f8e0f84f53f43591165b9bd803dcaec
osama <osama@akre.com>
2021-06-22 13:52:24 +0300

    fcm

--- dist/firebase-messaging-sw.js
+++ dist/firebase-messaging-sw.js
@@ -1,8 +1,34 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../firebase-messaging-sw.js')
-        .then(function(registration) {
-            console.log('Registration successful, scope is:', registration.scope);
-        }).catch(function(err) {
-        console.log('Service worker registration failed, error:', err);
-    });
-}
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyD-Ny42JvupIsCpgUSgYu__IcZry0ZEC8o",
+    authDomain: "fun-act.firebaseapp.com",
+    projectId: "fun-act",
+    storageBucket: "fun-act.appspot.com",
+    messagingSenderId: "545284794512",
+    appId: "1:545284794512:web:ffef95f1067025f7861015"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
