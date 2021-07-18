6991cab32d306e1fb0bc5f957de0c9ccb7dd5ccc
Osama <osama3akre@gmail.com>
2021-07-18 23:28:32 +0300

    test

--- dist/firebase-messaging-sw.js
+++ dist/firebase-messaging-sw.js
@@ -27,5 +27,5 @@ messaging.onBackgroundMessage((payload) => {
         body: payload.notification.body,
         icon: payload.notification.image,
     });
-    self.registration.hideNotification();
+    // self.registration.hideNotification();
 });
